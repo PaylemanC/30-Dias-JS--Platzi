214ff3df724bb8c3f8bc5c0b1814028c90b18571
Carina Payleman <rociocarina2@gmail.com>
2023-03-05 17:40:50 -0300

    Día 2, playground 2

---
+++ 2-calcula_propina.js
@@ -0,0 +1,6 @@
+function calculateTip(billAmount, tipPercentage) {
+    return (billAmount / 100) * tipPercentage;
+}
+  
+console.log(calculateTip(100, 10));
+console.log(calculateTip(1524.33, 25));
