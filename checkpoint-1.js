f7c0a0253255eb44100850558e2e1a7571dd1897
Carina Payleman <rociocarina2@gmail.com>
2023-03-13 19:39:21 -0300

    Checkpoint 1

---
+++ checkpoint-1.js
@@ -0,0 +1,13 @@
+function findLargestPalindrome(words) {
+    let palindrome = null;
+    for (let word of words) {
+        let reversedWord = word.split("").reverse().join("");
+        if (reversedWord === word && (palindrome == null || word.length > palindrome.length)) {
+            palindrome = word;
+        }
+    }
+    return palindrome;
+}
+
+console.log(findLargestPalindrome(["racecar", "level", "world", "hello"]));
+console.log(findLargestPalindrome(["Platzi", "javascript", "html", "css"]));
