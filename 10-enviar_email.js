a19988b3c6e2876888a51438c122f5ba1a490047
Carina Payleman <rociocarina2@gmail.com>
2023-04-01 10:50:47 -0300

    Día 11, playground 10

---
+++ 10-enviar_email.js
@@ -0,0 +1,35 @@
+function sendEmail(email, subject, body) {
+    const promise = new Promise((resolve, reject) => {
+      if (email && subject && body) {
+        window.setTimeout(() => {
+          const fullEmail = { email, subject, body };
+          resolve(fullEmail);
+        }, 2000)
+      } else {
+        window.setTimeout(() => {
+          reject(new Error('Error: Hacen falta campos para enviar el email'));
+        }, 2000)
+      }
+    });
+    return promise;
+}
+
+console.log(
+  sendEmail(
+    "test@mail.com",
+    "Nuevo reto",
+    "Únete a los 30 días de JS"
+  )
+  .then(result => console.log(result))
+)
+
+console.log(
+  sendEmail(
+    "test@mail.com",
+    "",
+    "Únete a los 30 días de JS"
+  )
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+)
+
