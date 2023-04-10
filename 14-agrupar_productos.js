525e8ffaf801d5079199d9cc9c0993239a617a3a
Carina Payleman <rociocarina2@gmail.com>
2023-04-10 14:05:34 -0300

    Día 13, playground 14

---
+++ 14-agrupar_productos.js
@@ -0,0 +1,26 @@
+function groupProducts(products, category) {
+    const filterProducts = products.filter(product => product.category === category);
+    let output = {
+        products: '', 
+        totalPrice: 0
+    }
+    let nameProducts = [];
+    filterProducts.forEach(product => { 
+        nameProducts.push(product.name);
+        output.totalPrice += product.price;
+    })
+    const totalProducts = nameProducts.join(', ');
+    output.products += totalProducts;
+
+    return output;
+}  
+
+const products = [
+  { name: "Smartphone", category: "Electronics", price: 800 },
+  { name: "Laptop", category: "Electronics", price: 1200 },
+  { name: "Shirt", category: "Clothing", price: 50 },
+  { name: "Pants", category: "Clothing", price: 100 },
+];
+
+groupProducts(products, "Electronics");
+groupProducts(products, "Clothing")
