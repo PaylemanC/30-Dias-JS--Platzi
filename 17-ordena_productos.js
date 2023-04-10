122c8fcc387595b59e76db6a69f6ff266312247e
Carina Payleman <rociocarina2@gmail.com>
2023-04-10 14:14:29 -0300

    Día 14, playground 17

---
+++ 17-ordena_productos.js
@@ -0,0 +1,15 @@
+function sortByAvailabilityAndPrice(products) {
+    const orderedProducts = [...products];
+    orderedProducts.sort((a, b) => a.price - b.price);
+    orderedProducts.sort((a, b) => b.inStock - a.inStock)
+    return orderedProducts;
+}
+
+const products = [
+    { name: "product1", price: 10, inStock: true },
+    { name: "product2", price: 20, inStock: false },
+    { name: "product3", price: 15, inStock: true },
+    { name: "product4", price: 5, inStock: false },
+]
+  
+sortByAvailabilityAndPrice(products);
