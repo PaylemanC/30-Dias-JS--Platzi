21d25501dd5eb5799119c03ccae77aa2259f6e63
Carina Payleman <rociocarina2@gmail.com>
2023-04-11 14:28:57 -0300

    Día 17, playground 19

---
+++ 19-modifica_array_prototype.js
@@ -0,0 +1,30 @@
+function arrayModified() {
+    Array.prototype.myFilter = function (callback) {
+      const filteredArray = [];
+      for (let i of this) {
+        if (callback(i)) {
+          filteredArray.push(i);
+        }
+      }
+      return filteredArray;
+    }
+}
+
+const array = [1,2,3,4,5,6]
+array.myFilter(num => num % 2 === 0)
+
+const arr = [
+	{
+	  name: "Juan",
+	  age: 10,
+	},
+	{
+	  name: "Pedro",
+	  age: 20,
+	},
+	{
+	  name: "Maria",
+	  age: 30,
+	},
+  ];
+array.myFilter((person) => person.age > 18)  
