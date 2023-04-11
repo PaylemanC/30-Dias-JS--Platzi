c2d7e3589fa16e66fee17368d90b0fba7ca145a9
Carina Payleman <rociocarina2@gmail.com>
2023-04-11 15:56:54 -0300

    Añade inputs de prueba

--- checkpoint-4.js
+++ checkpoint-4.js
@@ -75,3 +75,20 @@ class PremiumFlight extends Flight {
     return super.sellTicket(passenger);
   }
 }
+
+const flight = new Flight("CDMX", "Guadalajara", "2022-01-01", 5, 1000);
+const passenger = new Passenger("Juan", "Perez", 30);
+const reservation = flight.sellTicket(passenger);
+console.log(passenger.flights);
+console.log(flight.passengers);
+
+const economicFlight = new EconomicFlight(
+  "New York",
+  "Paris",
+  "2023-12-25",
+  100,
+  200
+);
+const passenger2 = new Passenger("Pedro", "Gutierrez", 17);
+const reservation2 = economicFlight.sellTicket(passenger2);
+console.log(reservation2.flight.price)
