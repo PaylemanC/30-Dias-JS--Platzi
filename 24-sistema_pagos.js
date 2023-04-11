7b9a360bb0bf6ef26c334d3866e2c9fc812d8510
Carina Payleman <rociocarina2@gmail.com>
2023-04-11 16:29:30 -0300

    Día 19, playground 24

---
+++ 24-sistema_pagos.js
@@ -0,0 +1,57 @@
+function processPay(method, quantity) {
+    return method.makePay(quantity)
+}
+
+class Pay {
+    makePay(quantity) {
+      return { realized: true, quantity: quantity }
+    }
+}
+
+class PayPal extends Pay {
+    constructor(email) {
+      super();
+      this.email = email;
+    }
+  
+    makePay(quantity) {
+      return { 
+        ...super.makePay(quantity),
+        platform: "PayPal", 
+        email: this.email
+      }
+    }
+}
+
+class Card extends Pay {
+    constructor(cardNumber) {
+      super();
+      this.cardNumber = cardNumber;
+    }
+  
+    makePay(quantity) {
+      if (this.cardNumber.length != 16) {
+        throw new Error("Número de tarjeta inválida.")
+      } else {
+        return {
+          ...super.makePay(quantity),
+          lastCardNumbers: this.cardNumber.substring(12, 17)
+        }
+      }
+    }
+}
+
+class Cash extends Pay {
+    constructor() {
+      super();
+    }
+}  
+
+const card = new Card("4913478952471122")
+console.log(processPay(card, 100));
+
+const paypal = new PayPal("test@mail.com")
+console.log(processPay(paypal, 240));
+
+const cash = new Cash()
+console.log(processPay(cash, 400));
